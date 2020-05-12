2a79768536a3e2e4cfc1f620b4064e1c020d170c
Your Name <you@example.com>
2020-05-12 11:46:32 +0300

    fix news all routs

--- resources/views/app/blocks/sidebar/left/news.blade.php
+++ resources/views/app/blocks/sidebar/left/news.blade.php
@@ -1,11 +1,13 @@
 <div class="container">
     <h3 class="row bg-custom-red justify-content-center text-white">НОВОСТИ</h3>
-    @foreach ($news as $oneNews)
-        @include('app.blocks.news.oneNews',$oneNews)
-        @if ($loop->last !== true)
-            <hr class="mb-1 mt-1">
-        @endif
-    @endforeach
+    @isset($news)
+        @foreach ($news as $oneNews)
+            @include('app.blocks.news.oneNews',$oneNews)
+            @if ($loop->last !== true)
+                <hr class="mb-1 mt-1">
+            @endif
+        @endforeach
+    @endisset
 
 </div>
 
