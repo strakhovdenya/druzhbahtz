5fc0988ccc2ea9fc935885201b675ac18328eb05
Your Name <you@example.com>
2020-06-06 07:59:23 +0300

    add versions

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js').sourceMaps().version();
+mix.sass('resources/sass/app.scss', 'public/css').sourceMaps().version();
+
 mix.options({ processCssUrls: false })
     .copy('resources/webfonts', 'public/webfonts');
