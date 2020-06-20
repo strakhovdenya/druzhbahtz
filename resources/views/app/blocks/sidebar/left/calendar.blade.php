99948ddec029b2e5ae818f94f245d890a57250bf
Your Name <you@example.com>
2020-06-20 12:44:33 +0300

    fix birthday title

--- resources/views/app/blocks/sidebar/left/calendar.blade.php
+++ resources/views/app/blocks/sidebar/left/calendar.blade.php
@@ -1,6 +1,4 @@
 <div class="container">
-    <h3 class="row bg-custom-blue justify-content-center text-white shadow">@lang('titles.birthday')</h3>
-
     <div class="row">
         @livewire('calendar.event')
     </div>
